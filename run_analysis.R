8a9adc626eb41be248def13fb1f620e5e38d8cb8
mkosla <kosla.3@buckeyemail.osu.edu>
2018-01-12 14:31:53 -0500

    load data from working directory

--- run_analysis.R
+++ run_analysis.R
@@ -2,9 +2,10 @@
 library(dplyr)
 library(data.table)
 
+
 #get variable names for testx from the features file
 
-features <- read.table("N:\\training\\Data Cleaning\\getdata_projectfiles_UCI HAR Dataset\\UCI HAR Dataset\\features.txt")
+features <- read.table(".\\getdata_projectfiles_UCI HAR Dataset\\UCI HAR Dataset\\features.txt")
 	colnames(features)[1] <- "varnum"
 	colnames(features)[2] <- "varname"
 
@@ -18,7 +19,7 @@ features <- read.table("N:\\training\\Data Cleaning\\getdata_projectfiles_UCI HA
 
 #get activity labels
 
-actlabel <- read.table("N:\\training\\Data Cleaning\\getdata_projectfiles_UCI HAR Dataset\\UCI HAR Dataset\\activity_labels.txt")
+actlabel <- read.table(".\\getdata_projectfiles_UCI HAR Dataset\\UCI HAR Dataset\\activity_labels.txt")
 	colnames(actlabel)[1] <- "activityid"
 	colnames(actlabel)[2] <- "activity"
 
@@ -161,14 +162,14 @@ TRAINmergedData = merge(merge_temp, trainx, by.x = "row", by.y="row")
 alldata <- subset(rbind(TESTmergedData, TRAINmergedData), select = -row)
 
 #export data
-write.table(alldata, file = "N:\\training\\Data Cleaning\\centraltendancyanddispersiondetailed.csv")
+write.table(alldata, file = ".\\centraltendancyanddispersiondetailed.csv")
 
 alldata2 <- aggregate(alldata[,3:88], list(alldata$subjectid, alldata$activity), mean)
 
 #subjectid and activity were renamed to Group.1 and Group.2, fix this below
 setnames(alldata2, c("Group.1", "Group.2"), c("subjectid", "activity"))
 
-write.table(alldata2, file = "N:\\training\\Data Cleaning\\centraltendancyanddispersionsummary.csv")
+write.table(alldata2, file = ".\\centraltendancyanddispersionsummary.csv")
 
 
 
